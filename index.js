facd878046c6f4bdbf6bd96a46166e597153e89e
Jim de Ronde <jim.de.ronde@greenhousegroup.com>
2023-03-23 13:44:26 +0100

    Trying ci/cd pipeline for clasp

---
+++ index.js
@@ -0,0 +1,17 @@
+function _getActiveSheet() {
+  try {
+    return SpreadsheetApp.getActiveSpreadsheet();
+  } catch (error) {
+    return undefined;
+  }
+}
+
+function toast(...args) {
+  const sheet = _getActiveSheet();
+
+  if (!sheet) {
+    throw new Error("No active sheet was found");
+  }
+
+  return sheet.toast(...args);
+}
